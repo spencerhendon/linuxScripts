97302b585cb5b86a35664e6c615d4acba7bffcb4
Spencer Hendon <spencerhendon@users.noreply.github.com>
2019-12-22 16:26:18 -0600

    Create updateScript.sh

---
+++ updateScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# UPDATE SCRIPT
+
+# CHECK IF LOG FOLDER EXIST
+# IF DOES NOT EXIST... CREATE IT
+if [ -d ~/updateLogs ]
+then
+        mkdir ~/updateLogs
+fi
+
+# UPDATE AND UPGRADE
+apt update > ~/updateLogs/$(date "+%F-%T")_update
+apt upgrade -y > ~/updateLogs/$(date "+%F-%T")_upgrade
+
+if [ -f /var/run/reboot-required ]
+then
+        echo "/var/run/reboot-required exists. REBOOTING." | tee ~/updateLogs/$(date "+%F-%T")_reboot
+        reboot
+fi
